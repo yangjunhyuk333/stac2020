127dd4deb2f342203aaf145cd0afddf524f88d27
YJH <chuchoun1234@gmail.com>
2020-06-10 12:02:05 +0900

    CleanUp Comment

--- stac2020.ino
+++ stac2020.ino
@@ -4,11 +4,9 @@
 
 SoftwareSerial BTSerial(1, 0); //블루투스 모듈 HC-06 모듈 핀 설정 (TX, RX)
 
-MPU6050 mpu6050(Wire);
+MPU6050 mpu6050(Wire); //mpu6050 객체 생성
 
-float X;
-float calX;
-
+float calX; //mpu6050에서 받아온 x값을 저장하는 변수
 
 void setup() {               
   Serial.begin(9600); //pc와 연결
@@ -16,35 +14,42 @@ void setup() {
   //블루투스 연결
   BTSerial.begin(9600);
 
+  //mpu6050 라이브러리 시작
   Wire.begin();
   mpu6050.begin();      
 }
 
 void loop() {
-  calAngleX();
+  calAngleX(); //X을 통해서 척추의 각도를 측정하는 함수를 실행
 }
 
 //------------------------------------------------
 
 //X값을 통해서 척추의 각도를 측정하는함수
-
 void calAngleX(){
-  mpu6050.update();
-  calX = mpu6050.getAccAngleX();
-  Serial.print("calX: ");
+  mpu6050.update(); //mpu6050 값을 업데이트 해줌
+  calX = mpu6050.getAccAngleX(); //mpu6050에서 받아온 각도를 저장하는 과정
+  Serial.print("calX: "); 
   Serial.println(calX);
   delay(100);
+
+  //각도를 비교해서 -60도가 넘어가면 코드 실행
   if(calX > -60){
+
     delay (2000);
     mpu6050.update();
     calX = mpu6050.getAccAngleX();
-    if(calX > -60){
-      analogWrite(3, 200);
-      BTSerial.println("10");
+
+    //다시한번 각도를 비교해서 -60도가 넘어가면 코드 실행(척추 각도 비정상)
+    if(calX > -60){ 
+      analogWrite(3, 200); //진동을 울린다.
+      BTSerial.println("10"); //안드로이드로 10을 블루투스로 전송한다.
       delay (1000);
-      analogWrite(3, 0);
+      analogWrite(3, 0); //진동을 끈다.
     }
-  }else{
-    BTSerial.println("20");
+  }
+  //-60도를 넘어가지 않았다면 코드 실행(척추 각도 정상)
+  else{ 
+    BTSerial.println("20"); //안드로이드로 20을 전송한다.
   }
 }
